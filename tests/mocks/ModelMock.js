819ada6b1f57cc341fa51a21819be8e9bc1392a4
Esteban <edjeordjian@fi.uba.ar>
2023-04-09 14:52:35 -0300

    Added mock for tests

--- tests/mocks/ModelMock.js
+++ tests/mocks/ModelMock.js
@@ -7,6 +7,21 @@ class ModelMock {
         return [];
     }
 
+    async findAll(obj1, obj2) {
+        return [];
+    }
+
+    async create(obj1, obj2) {
+        return [];
+    }
+
+    async update(obj1, obj2) {
+        return [];
+    }
+
+    async destroy(obj1, obj2) {
+        return [];
+    }
 }
 
 module.exports = {
