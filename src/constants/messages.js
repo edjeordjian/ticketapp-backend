d27d549acefaea6a35510c85afdc67810cb24721
Esteban <edjeordjian@fi.uba.ar>
2023-05-13 14:09:49 -0300

    Block user

--- src/constants/messages.js
+++ src/constants/messages.js
@@ -1,5 +1,9 @@
 const OK_LBL = "Ok";
 
+const BLOCKED_USER = "Usuario suspendido";
+
+const ACTIVATED_USER = "Usuario activado"
+
 module.exports = {
-    OK_LBL
+    OK_LBL, BLOCKED_USER, ACTIVATED_USER
 };
--- src/data/migrations/migrations.js
+++ src/data/migrations/migrations.js
@@ -30,17 +30,6 @@ const today = new Date();
 
 async function runMigrations() {
     logInfo(RUNNING_MIGRATIONS_LBL);
-
-    await queryInterface.addColumn(User.tableName,
-        "is_blocked",
-        {
-            type: Sequelize.BOOLEAN,
-            allowNull: false,
-            validate: { notEmpty: true },
-            defaultValue: false
-        }).catch(err => {
-        console.log(err.toString());
-    });
 }
 
 module.exports = {
